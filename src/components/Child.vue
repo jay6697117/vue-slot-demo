a27a6c45e75d1304ab81f94b9cd3e0e2f84bc1ac
steve zhang <871506263@qq.com>
2022-04-05 02:22:17 +0800

    提交代码于: 2022-04-05 02:22:17

--- src/components/Child.vue
+++ src/components/Child.vue
@@ -1,13 +1,35 @@
 <template>
   <div class="child">
-    <input :value="propName" @input="inputFn" type="text" />
-    <div>propName:{{ propName }}</div>
+    <!-- <input :value="propName" @input="inputFn" type="text" />
+    <div>子propName:{{ propName }}</div> -->
+    <input :value="name" @input="inputFn" type="text" />
+    <div>子name:{{ name }}</div>
+    <hr />
+    <!-- <input :value="propName1" @input="inputFn1" type="text" />
+    <div>子propName1:{{ propName1 }}</div> -->
+    <input :value="name1" @input="inputFn1" type="text" />
+    <div>子name1:{{ name1 }}</div>
   </div>
 </template>
 <script>
 export default {
   props: {
-    propName: {
+    // propName: {
+    //   type: String,
+    //   default: '',
+    //   required: true
+    // },
+    // propName1: {
+    //   type: String,
+    //   default: '',
+    //   required: true
+    // },
+    name: {
+      type: String,
+      default: '',
+      required: true
+    },
+    name1: {
       type: String,
       default: '',
       required: true
@@ -16,7 +38,13 @@ export default {
   methods: {
     inputFn(e) {
       console.log('input 改变:', e.target.value);
-      this.$emit('update:propName', e.target.value);
+      // this.$emit('update:propName', e.target.value);
+      this.$emit('update:name', e.target.value);
+    },
+    inputFn1(e) {
+      console.log('input 改变 1:', e.target.value);
+      // this.$emit('update:propName1', e.target.value);
+      this.$emit('update:name1', e.target.value);
     }
   }
 };
--- src/components/Father.vue
+++ src/components/Father.vue
@@ -1,10 +1,17 @@
 <template>
   <div>
-    <Child :propName.sync="name"></Child>
-    <!-- <Child :propName="name" @update:propName="updateFn"></Child> -->
-    <button @click="alertFn">点击</button>
-    <button @click="changeFn">改变</button>
-    <div>name:{{ name }}</div>
+    <Child v-bind.sync="obj"></Child>
+    <!-- <Child :propName.sync="name" :propName1.sync="name1"></Child> -->
+    <!-- <Child :propName="name" @update:propName="updateFn" :propName1="name1" @update:propName1="updateFn1"></Child> -->
+    <button @click="alertFn">alertFn</button>
+    <button @click="changeFn">changeFn</button>
+    <!-- <div>父name:{{ name }}</div> -->
+    <div>父name:{{ obj.name }}</div>
+    <hr />
+    <button @click="alertFn1">alertFn1</button>
+    <button @click="changeFn1">changeFn1</button>
+    <!-- <div>父name:{{ name1 }}</div> -->
+    <div>父name:{{ obj.name1 }}</div>
   </div>
 </template>
 
@@ -17,19 +24,40 @@ export default {
   },
   data() {
     return {
-      name: 'xxx'
+      // name: '1',
+      // name1: '2',
+      obj: { name: '1', name1: '2' }
     };
+  },
+  created() {
+    console.log('this:', this);
+    console.log('this.$options.data():', this.$options.data());
   },
   methods: {
     alertFn() {
-      alert(this.name);
+      // alert(this.name);
+      alert(this.obj.name);
+    },
+    alertFn1() {
+      // alert(this.name1);
+      alert(this.obj.name1);
     },
     changeFn() {
-      this.name = '123';
+      // this.name = '111';
+      this.obj.name = '111';
     },
-    updateFn(val) {
-      this.name = val;
-    }
+    changeFn1() {
+      // this.name1 = '222';
+      this.obj.name1 = '222';
+    },
+    // updateFn(val) {
+    //   // this.name = val;
+    //   this.obj.name = val;
+    // },
+    // updateFn1(val) {
+    //   // this.name1 = val;
+    //   this.obj.name1 = val;
+    // }
   }
 };
 </script>
